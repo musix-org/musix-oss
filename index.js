60db516870b6e9de27458d939f7ed34141c50148
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2019-05-30 21:14:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ client.on('message', async msg => {
 						msg.channel.send(`
 	__**Song selection:**__
 ${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
-	Please provide a value to select one of the search results ranging from 1-10.
+Please provide a value to select one of the search results ranging from __1-10__.
 						`);
 						// eslint-disable-next-line max-depth
 						try {
