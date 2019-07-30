0e0d90c85908685b78b0ef2c448d48728153905c
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2019-07-30 16:16:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('message', async msg => {
 		msg.channel.send('-help to see my commands.')
 	}
 	if (msg.content.startsWith('>')) {
-		if (msg.author.id !== '360363051792203779') msg.channel.send(':x: Developement commands are not avaiable for public usage!')
+		if (msg.author.id !== '360363051792203779' || msg.author.id === '384002606621655040') return msg.channel.send(':x: Developement commands are not avaiable for public usage!')
 		if (msg.author.id === '360363051792203779' || msg.author.id === '384002606621655040') {
 			if (msg.content === '>devstop') {
 				serverQueue.songs = [];
@@ -27,8 +27,8 @@ client.on('message', async msg => {
 				const args = msg.content.slice(6)
 				msg.channel.send(eval(args));
 			}
+			return
 		}
-		return
 	}
 	if (msg.content.startsWith(`${PREFIX}`)) {
 		var guildms = client.guilds.find(x => x.name === 'Musix Support')
