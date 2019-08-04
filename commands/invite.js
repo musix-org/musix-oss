3388da95b97b04a6e30abb1b3dd7802a7e8982e6
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2019-08-04 16:02:31 +0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,8 +4,8 @@ module.exports = {
     cooldown: 5,
     execute(message, args, client, RichEmbed) {
         const embed = new RichEmbed()
-            .setTitle('Commands for Musix!')
-            .url('https://bit.ly/2YDrKgh')
+            .setTitle('Invite Musix!')
+            .setURL('https://bit.ly/2YDrKgh')
             .setAuthor(client.user.username, client.user.displayAvatarURL)
             .setColor('#b50002')
         return message.channel.send(embed);
