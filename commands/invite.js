29b40867a3ca1446c46b469ecfad1b5d59089628
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2019-08-04 16:05:06 +0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,9 +4,8 @@ module.exports = {
     cooldown: 5,
     execute(message, args, client, RichEmbed) {
         const embed = new RichEmbed()
-            .setTitle('Invite Musix!')
+            .setTitle('Invite Musix to your Discord server!')
             .setURL('https://bit.ly/2YDrKgh')
-            .setAuthor(client.user.username, client.user.displayAvatarURL)
             .setColor('#b50002')
         return message.channel.send(embed);
     }
