271c16a4c064568b1c75618e649674ca1f28d761
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2019-10-12 13:38:12 +0300

    removed console.lgos

--- funcs/handleVideo.js
+++ funcs/handleVideo.js
@@ -1,6 +1,5 @@
 module.exports = async function (video, message, voiceChannel, client, playlist = false) {
     const Discord = require('discord.js');
-    console.log("handlevideo function")
     let song = {
         id: video.id,
         title: Discord.Util.escapeMarkdown(video.title),
@@ -8,7 +7,6 @@ module.exports = async function (video, message, voiceChannel, client, playlist 
     }
     const serverQueue = client.queue.get(message.guild.id);
     if (client.global.db.guilds[message.guild.id].defaultVolume === undefined) {
-        console.log("defaultvolume undefined")
         client.global.db.guilds[message.guild.id] = {
             prefix: client.global.db.guilds[message.guild.id].prefix,
             defaultVolume: 5,
@@ -16,7 +14,6 @@ module.exports = async function (video, message, voiceChannel, client, playlist 
         return message.channel.send(':x: `Error:` the default volume is undefined for this server. Please try again after a while.');
     }
     if (!serverQueue) {
-        console.log("no serverQueue")
         const construct = {
             textChannel: message.channel,
             voiceChannel: voiceChannel,
@@ -26,23 +23,17 @@ module.exports = async function (video, message, voiceChannel, client, playlist 
             playing: true,
             looping: false
         };
-        console.log("Pushing song")
         construct.songs.push(song);
-        console.log("settings queue")
         client.queue.set(message.guild.id, construct);
         try {
-            console.log("connecting...")
             var connection = await voiceChannel.join();
             construct.connection = connection;
-            console.log("connected")
             client.funcs.play(message.guild, construct.songs[0], client, message, 0);
         } catch (error) {
-            console.log("error with connection")
             client.queue.delete(message.guild.id);
             return message.channel.send(`:x: An error occured: ${error}`);
         }
     } else {
-        console.log("adding to queue")
         serverQueue.songs.push(song);
         if (playlist) return undefined;
         return message.channel.send(`:white_check_mark: **${song.title}** has been added to the queue!`);
