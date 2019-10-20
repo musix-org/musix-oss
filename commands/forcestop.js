50821c2b2b9a08fb2648addd4ba2d41332445b19
Matte <51192395+MatteZ02@users.noreply.github.com>
2019-10-20 20:46:57 +0300

    Update forcestop.js

--- commands/forcestop.js
+++ commands/forcestop.js
@@ -1,9 +1,10 @@
 module.exports = {
-    name: 'stop',
-    description: 'Stop command.',
+    name: 'forcestop',
+    description: 'force stop command.',
     cooldown: 5,
     execute(message, args, client, Discord, prefix) {
         if (message.author.id !== '360363051792203779') return message.channel.send(':x: You are not allowed to do that!')
         client.queue.delete(guild.id);
+        message.channel.send('queue deleted')
     }
 };
