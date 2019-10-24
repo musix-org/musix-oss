7162be204daba03abb4d1fde44932983a8352fbd
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2019-10-24 20:21:55 +0300

    Update settings.js

--- commands/settings.js
+++ commands/settings.js
@@ -83,14 +83,22 @@ module.exports = {
         client.global.db.guilds[message.guild.id].dj = false;
         message.channel.send(':white_check_mark: `DJ` now set to `false`');
       }
+    } else if (args[1] === 'announcesongs') {
+      if (client.global.db.guilds[message.guild.id].startPlaying) {
+        client.global.db.guilds[message.guild.id].startPlaying = false;
+        return message.channel.send(':white_check_mark: announcesong set to `false`!');
+      } else {
+        client.global.db.guilds[message.guild.id].startPlaying = true;
+        return message.channel.send(':white_check_mark: announcesong set to `true`!');
+      }
     } else {
       const embed = new Discord.RichEmbed()
         .setTitle('Guild settings for Musix')
         .addField('prefix', 'Change the guild specific prefix. (string)', true)
         .addField('volume', 'Change the default volume that the bot will start playing at. (number)', true)
-        .addField('permissions', 'Change whether to require permissions to use eg `skip, stop, pause, loop, etc... (boolean)`', true)
-        .addField('setdj', 'Set a DJ role. This will allow chosen users to freely use all Musix commands. This will automatically set the `permissions` settings to true in order for the `DJ` role to have effect! (boolean)', true)
-        .addField('announce songs', 'Whether to announce songs that start playing or not.')
+        .addField('permissions', 'Change whether to require permissions to use eg `skip, stop, pause, loop, etc...`', true)
+        .addField('setdj', 'Set a DJ role. This will allow chosen users to freely use all Musix commands. This will automatically set the `permissions` settings to true in order for the `DJ` role to have effect!', true)
+        .addField('announcesongs', 'Whether to announce songs that start playing or not.')
         .setFooter(`how to use: ${prefix}settings <Setting name> <value>`)
         .setAuthor(client.user.username, client.user.displayAvatarURL)
         .setColor('#b50002')
