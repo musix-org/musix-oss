c32d592074771007dd65f9aacde543515cdcfa2c
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2019-11-18 20:36:18 +0200

    fixed songSelection setting

--- commands/settings/songSelection.js
+++ commands/settings/songSelection.js
@@ -1,11 +1,11 @@
 module.exports = {
     name: 'songselection',
     async execute(message, args, client, Discord, prefix) {
-        if (!client.global.db.guilds[message.guild.id].dj) {
+        if (!client.global.db.guilds[message.guild.id].songSelection) {
             message.channel.send(':white_check_mark: Songselection now set to `true`!');
-            client.global.db.guilds[message.guild.id].dj = true;
+            client.global.db.guilds[message.guild.id].songSelection = true;
         } else {
-            client.global.db.guilds[message.guild.id].dj = false;
+            client.global.db.guilds[message.guild.id].songSelection = false;
             message.channel.send(':white_check_mark: Songselection now set to `false`');
         }
     }
