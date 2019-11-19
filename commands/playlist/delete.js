3d391a685a5e63dcec974d2266babdb62365b861
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2019-11-19 18:08:00 +0200

    playlist patch

---
+++ commands/playlist/delete.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'setpremium',
+    async execute(message, args, client, Discord, prefix) {
+        client.global.db.playlists[message.guild.id] = {
+            songs: [],
+            firstSong: undefined,
+            saved: false,
+        };
+        message.channel.send(':white_check_mark: This guild is now premium! :tada:')
+    }
+};
--- commands/playlist/remove.js
+++ commands/playlist/remove.js
@@ -5,7 +5,6 @@ module.exports = {
             if (!args[2]) return message.channel.send(':x: Please provide a number on the position of the song that you wan\'t to remove!');
             const songNum = parseInt(args[2]) - 1;
             if (isNaN(songNum)) return message.channel.send(':x: You need to enter a __number__!');
-            if (songNum === 0) return message.channel.send(':x: You can not remove the currently playing song!');
             if (parseInt(songNum) > client.global.db.playlists[message.guild.id].songs.size) return message.channel.send(`:x: There is only ${serverQueue.songs.size} amount of songs in the queue!`);
             message.channel.send(`🗑️ removed \`${client.global.db.playlists[message.guild.id].songs[songNum].title}\` from the playlist!`);
             return client.global.db.playlists[message.guild.id].songs.splice(songNum, 1);
