069ba70cb592850d5b3275bd6c763603e50b8257
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2019-12-06 15:29:42 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,5 +3,7 @@ module.exports = {
     api_key: process.env.API_KEY,
     debug_channel: "634718645188034560",
     devId: "360363051792203779",
-    prefix: ">"
+    prefix: ">",
+    embedColor: "#b50002",
+    invite: "https://discordapp.com/api/oauth2/authorize?client_id=607266889537945605&permissions=271600640&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Foauth2%2Fauthorize%3Fclient_id%3D607266889537945605%26%3Bscope%3Dbot%26%3Bpermissions%3D0&scope=bot",
 }
