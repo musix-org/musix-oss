4a74bc8871591f619497df46575326cedcca67ee
Matte <51192395+MatteZ02@users.noreply.github.com>
2020-01-21 15:08:55 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     token: process.env.TOKEN,
-    api_key: process.env.API_KEY2,
+    api_key: process.env.API_KEY,
     debug_channel: "634718645188034560",
     devId: "360363051792203779",
     prefix: ">",
