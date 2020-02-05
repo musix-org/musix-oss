2f81d6f1a9746117901cf4896a4034e875a89d50
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-02-05 22:10:20 +0200

    Update config.js

--- src/struct/config/config.js
+++ src/struct/config/config.js
@@ -6,10 +6,10 @@ module.exports = {
     debug_channel: "634718645188034560",
     devId: "360363051792203779",
     //misc
-    embedColor: "#b50002",
-    invite: "https://discordapp.com/api/oauth2/authorize?client_id=607266889537945605&permissions=271600640&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Foauth2%2Fauthorize%3Fclient_id%3D607266889537945605%26%3Bscope%3Dbot%26%3Bpermissions%3D0&scope=bot",
+    embedColor: "#B127CD",
+    invite: "https://discordapp.com/api/oauth2/authorize?client_id=649252971690983446&permissions=8&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Foauth2%2Fauthorize%3Fclient_id%3D649252971690983446%26%3Bscope%3Dbot%26%3Bpermissions%3D0&scope=bot",
     //Settings
-    devMode: true,
+    devMode: false,
     dblApi: false,
     saveDB: false,
     //db values
