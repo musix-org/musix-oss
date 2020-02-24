5a5ee2b324b411dd2623049ead6089e0abcac74f
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-02-24 20:36:48 +0200

    Update skipto.js

--- commands/skipto.js
+++ commands/skipto.js
@@ -10,7 +10,7 @@ module.exports = {
         const serverQueue = client.queue.get(msg.guild.id);
         if (client.funcs.check(client, msg, command)) {
             if (!args[1]) return msg.channel.send(`<:redx:674263474704220182> correct usage: \`${command.usage}\``);
-            const point = parseInt(args[1] - 1);
+            const point = parseInt(args[1]);
             if (isNaN(point)) return msg.channel.send('<:redx:674263474704220182> I\'m sorry, But you need to enter a valid __number__.');
             if (point > serverQueue.songs.size) return msg.channel.send('<:redx:674263474704220182> That song does not exist!');
             if (point < 1) return msg.channel.send('<:redx:674263474704220182> You can\'t skip to the song currently playing!');
