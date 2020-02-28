3e2dc2fcb92421733f4192e208381664f65fba41
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-02-28 16:11:33 +0200

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -10,9 +10,9 @@ module.exports = {
         const serverQueue = client.queue.get(msg.guild.id);
         if (client.funcs.check(client, msg, command)) {
             if (!args[1]) return msg.channel.send('<:redx:674263474704220182> Please provide a song position in queue for me to remove!');
-            const pos = parseInt(args[1]);
+            const pos = parseInt(args[1] - 1);
             if (isNaN(pos)) return msg.channel.send('<:redx:674263474704220182> You need to enter a number!');
-            if (pos === 0) return msg.channel.send('<:redx:674263474704220182> You can not remove the currently playing song!');
+            if (pos < 1) return msg.channel.send('<:redx:674263474704220182> That songs does not exist!');
             if (pos > serverQueue.songs.length) return msg.channel.send(`<:redx:674263474704220182> There is only ${serverQueue.songs.length} amount of songs in the queue!`);
             msg.channel.send(`🗑️ removed \`${serverQueue.songs[pos].title}\` from the queue!`);
             return serverQueue.songs.splice(pos, 1);
