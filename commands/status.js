55cddcbb42d311b31358a89abc18987ae4916f7b
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-03-04 18:04:29 +0200

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -16,8 +16,6 @@ module.exports = {
                 .addField(':signal_strength: Ping', client.ws.ping, true)
                 .addField('Latency', latency, true)
                 .addField(':stopwatch: Uptime', uptime, true)
-                .addField(`:play_pause: Currently playing music on`, `${client.voice.connections.size} guild(s)`, true)
-                .addField(`💿 Operating system`, process.platform, true)
                 .setAuthor(client.user.username, client.user.displayAvatarURL)
                 .setColor(client.config.embedColor)
             m.delete();
