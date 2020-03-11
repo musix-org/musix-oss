6b50165e6ffa046c135a3368e6a169314c2fca18
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-03-11 17:37:47 +0200

    Update exe.js

--- struct/funcs/exe.js
+++ struct/funcs/exe.js
@@ -1,7 +1,7 @@
 module.exports = function (msg, args, client, Discord, prefix, command) {
     const permissions = msg.channel.permissionsFor(msg.client.user);
     if (!permissions.has('EMBED_LINKS')) return msg.channel.send('<:redx:674263474704220182> I cannot send embeds (Embed links), make sure I have the proper permissions!');
-    if (!permissions.has('USE_EXTERNAL_EMOJIS')) return msg.channel.send('<:redx:674263474704220182> I cannot use external emojis, make sure I have the proper permissions!'); DEPRACATED!
+    if (!permissions.has('USE_EXTERNAL_EMOJIS')) return msg.channel.send('<:redx:674263474704220182> I cannot use external emojis, make sure I have the proper permissions!');
     try {
         command.uses++;
         command.execute(msg, args, client, Discord, prefix, command);
