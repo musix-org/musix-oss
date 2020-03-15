77195df26736d07f61d63411bb40b46385775845
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-03-15 21:57:28 +0200

    Update ffmpeg.js

--- struct/funcs/ffmpeg.js
+++ struct/funcs/ffmpeg.js
@@ -1,9 +1,9 @@
 module.exports = async function (client) {
+    if (!client.guild.find('489111553321336832')) return;
     try {
         await client.channels.fetch(client.config.secondary_test_channel)
             .then(x => x.join());
     } catch (error) {
-        if (error === "TypeError: Cannot read property 'join' of null") return;
         client.debug_channel.send(client.messages.errorDetected + error);
     }
 };
