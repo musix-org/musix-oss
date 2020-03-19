262917d623d212fdb0c1a708eb026fcd57aab24e
Matte <51192395+MatteZ02@users.noreply.github.com>
2020-03-19 20:25:38 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -42,6 +42,7 @@ module.exports = {
 					var video = await youtube.getVideoByID(videos[0].id);
 				} catch (err) {
 					console.error(err);
+					client.debug_channel.send(err);
 					if (err.code === 403) {
 						if (client.config.api_key === client.config.api_key) {
 							client.config.api_key = client.config.api_key2;
