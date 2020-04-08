1beb0fa86a65897b75cfe510d54b83bf71bf5622
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-04-08 20:48:57 +0300

    nigthCore added

---
+++ src/commands/nigthcore.ts
@@ -0,0 +1,26 @@
+module.exports = {
+  name: "nigthcore",
+  alias: "nc",
+  usage: "<true/false>",
+  description: "Change nigthcore on/off",
+  onlyDev: false,
+  permission: "MANAGE_MESSAGES",
+  category: "music",
+  async execute(msg, args, client, Discord, prefix, command) {
+    const queue = client.queue.get(msg.guild.id);
+    if (!args[1] && queue)
+      return msg.channel.send(
+        `${client.messages.currentNigthCore}**${queue.nigthCore}**`
+      );
+    if (client.funcs.check(client, msg, command)) {
+      if (args[1] === "true") {
+        queue.nigthCore = true;
+      } else if (args[1] === "false") {
+        queue.nigthCore = false;
+      }
+      let message;
+      message = client.messages.nigthCoreApplied.replace("%BOOLEAN%", args[1]);
+      return msg.channel.send(message);
+    }
+  },
+};
