a35318cea813d0ec6d9953316ee3ef273437c37a
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-04-15 10:20:45 +0300

    updated ytdl and streamOps

--- src/struct/config/streamConfig.ts
+++ src/struct/config/streamConfig.ts
@@ -1,4 +1,9 @@
 module.exports = {
-    ytdlOptions: { filter: "audio", highWaterMark: 1 << 25, volume: false, begin: null },
-    options: { seek: 0, bitrate: 1024, passes: 10, volume: 1, type: "converted" }
+  ytdlOptions: { filter: "audio", highWaterMark: 1 << 10, volume: false },
+  options: {
+    seek: 0,
+    bitrate: 1024,
+    volume: 1,
+    type: "converted",
+  },
 };
