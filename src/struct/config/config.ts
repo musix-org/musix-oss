27a8b7f34bc10d2cd0a4b081aec2a5357b9aca4c
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-04-16 23:12:29 +0300

    Update config.ts

--- src/struct/config/config.ts
+++ src/struct/config/config.ts
@@ -13,7 +13,7 @@ module.exports = {
   invite:
     "https://discordapp.com/oauth2/authorize?client_id=607266889537945605&permissions=3427328&scope=bot",
   supportServer: "https://discord.gg/rvHuJtB",
-  devMode: true,
+  devMode: false,
   api: false,
   saveDB: true,
   respawn: true,
