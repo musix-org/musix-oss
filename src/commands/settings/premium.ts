2d55a3a8da0098825d5c02a6773735d936027693
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-04-19 23:26:31 +0300

    Update premium.ts

--- src/commands/settings/premium.ts
+++ src/commands/settings/premium.ts
@@ -3,7 +3,8 @@ module.exports = {
   async execute(msg, args, client) {
     if (!args[2])
       return msg.channel.send(
-        client.messages.premiumState + client.global.db.guilds[args[2]].premium
+        client.messages.premiumState +
+          client.global.db.guilds[msg.guild.id].premium
       );
     if (msg.member.id !== client.config.devId)
       return msg.channel.send(client.messages.onlyDev);
