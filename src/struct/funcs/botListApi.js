cd15edbedc73b7ca640e9218dcd144218d2b5ef5
MatteZ02 <47610069+MatteZ02@users.noreply.github.com>
2020-04-25 19:20:19 +0300

    Update botListApi.js

--- src/struct/funcs/botListApi.js
+++ src/struct/funcs/botListApi.js
@@ -11,7 +11,7 @@ module.exports = function (client) {
         .then((results) => {
             guildCount = results.reduce((prev, val) => prev + val, 0);
             dbl.postStats(guildCount * 7, client.shard.ids, client.config.shards);
-            bod.postStats(guildCount * 7, client.config.shards)
+            bod.postStats(guildCount * 7, client.shard.ids, client.config.shards)
         })
         .catch(console.error);
 
@@ -34,6 +34,6 @@ module.exports = function (client) {
         }*/
     setInterval(() => {
         dbl.postStats(guildCount * 7, client.shard.ids, client.config.shards);
-        bod.postStats(guildCount * 7, client.config.shards)
+        bod.postStats(guildCount * 7, client.shard.ids, client.config.shards)
     }, 1800000);
 };
